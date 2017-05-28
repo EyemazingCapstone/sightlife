eb43ed7ab09ee052712e9ef462048ed1c78e6924
Sarah Stanton <sarah.g.stanton@gmail.com>
2017-05-28 07:44:04 -0700

    legend, benefits

--- js/LineChart.js
+++ js/LineChart.js
@@ -3,7 +3,7 @@ var LineChart = function() {
 
     var margin = {
         top: 10,
-        right: 50,
+        right: 200,
         bottom: 40,
         left: 50
     };
@@ -91,12 +91,44 @@ var LineChart = function() {
 
         var ele = d3.select(this);
         var svg = ele.selectAll('svg').data(data)
+
+
+
+
         
         console.log('here');
         var svgEnter = svg.enter()
             .append('svg')
             .attr('width', width)
             .attr('height', height);
+
+
+    
+        var ordinal = d3.scaleOrdinal()
+            .domain(["No policy", "Mandatory Notification", "MN, First Person Consent"])
+            .range([ "#BF6059", "#499996", "#E2A855"]);
+
+        //var svg2 = d3.selectAll("svg");
+
+        svgEnter.append("g")
+            //.attr("class", "legendQuant")
+            .attr("transform", "translate(" + (drawWidth + margin.right / 3)+ "," + 5 + ")")
+            .append('g')
+            .attr('class','legendOrdinal');
+
+        var legend = d3.legendColor()
+            //.labelFormat(d3.format(".2f"))
+            //.labels(d3.legendHelpers.thresholdLabels)
+            //.labels(["No policy", "Mandatory Notification", "MN and First Person Consent"])
+            .labelWrap(30)
+            .shapeWidth(20)
+            //.labelAlign("start")
+            //.orient('horizontal')
+            .scale(ordinal);
+
+        svgEnter.select(".legendOrdinal")
+            .call(legend);
+
 
         // Append g for holding chart markers
         svgEnter.append("g")
@@ -186,7 +218,7 @@ var LineChart = function() {
             .attr("stroke", '#BF6059')
             .attr("stroke-dashoffset", function(d){
                 var LENGTH = d3.select(this).node().getTotalLength()
-                return -LENGTH;
+                return LENGTH;
             })
             .transition()
             .duration(2000)
@@ -232,7 +264,7 @@ var LineChart = function() {
             .attr("stroke", '#499996')
             .attr("stroke-dashoffset", function(d){
                 var LENGTH = d3.select(this).node().getTotalLength()
-                return -LENGTH;
+                return LENGTH;
             })
             .transition()
             .duration(2000)
@@ -277,7 +309,7 @@ var LineChart = function() {
             .attr("stroke", '#E2A855')
             .attr("stroke-dashoffset", function(d){
                 var LENGTH = d3.select(this).node().getTotalLength()
-                return -LENGTH;
+                return LENGTH;
             })
             .transition()
             .duration(2000)
@@ -451,113 +483,3 @@ var LineChart = function() {
 }
 
 
-/*var TreeMap = function(root) {
-    // Set default values
-    var height = 610,
-        width = 960,
-        margin = {
-            left: 70,
-            bottom: 50,
-            top: 30,
-            right: 10,
-        },
-        title = "My Treemap";
-
-    let color = d3.scaleOrdinal(d3.schemeCategory20);
-
-    var treemap = d3.treemap()
-        .tile(d3.treemapResquarify)
-        .size([width, height])
-        .round(true)
-        .paddingInner(1);
-        
-    treemap(root);
-
-
-    // Function returned by TreeMap
-    var chart = function(selection) {
-        // Height/width of the drawing area itself
-        var chartHeight = height - margin.bottom - margin.top;
-        var chartWidth = width - margin.left - margin.right;
-
-        // Iterate through selections, in case there are multiple
-        selection.each(function(data) {
-            // Use the data-join to create the svg (if necessary)
-            var ele = d3.select(this);
-            var svg = ele.selectAll("svg").data([root.leaves()]);
-
-            // Append static elements (i.e., only added once)
-            var svgEnter = svg.enter()
-                .append("svg")
-                .attr('width', width)
-                .attr("height", height);
-
-            svgEnter.append("text")
-                .attr("x", width / 2 )
-                .attr("y",  margin.top )
-                .style("text-anchor", "middle")
-                .text(title)
-
-            // g element for markers
-            svgEnter.append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + (margin.top + 40) + ')')
-                .attr("class", 'chartG');
-
-            var rects = ele.select('.chartG').selectAll('rect').data(root.leaves(), (d) => d.id);
-
-            rects.enter().append("rect")
-                .attr("id", function(d) { 
-                    return d.data.id; 
-                })
-                .attr("x", function(d){
-                    return d.x0;
-                })
-                .attr("y", function(d){
-                    return d.y0;
-                })
-                .attr("width", function(d) { 
-                    return d.x1 - d.x0; 
-                })
-                .attr("height", function(d) { 
-                    return d.y1 - d.y0; 
-                })
-                .attr("fill", function(d) { 
-                    return color(d.parent.data.id); 
-                })
-                .append("title")
-                .text(function(d) {
-                    return d.data.name; 
-                });
-
-        });
-    };
-
-    // Getter/setter methods to change locally scoped options
-    chart.height = function(value) {
-        if (!arguments.length) return height;
-        height = value;
-        return chart;
-    };
-
-    chart.width = function(value) {
-        if (!arguments.length) return width;
-        width = value;
-        return chart;
-    };
-
-    // allow user to change color
-    chart.color = function(value) {
-        if (!arguments.length) return color;
-        color = d3.scaleOrdinal(value);
-        return chart;
-    };
-
-    // allow user to change chart title
-    chart.title = function(value){
-        if (!arguments.length) return title;
-        title = value;
-        return chart;
-    };
-
-    return chart;
-};*/
